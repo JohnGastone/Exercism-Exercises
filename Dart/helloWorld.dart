743437381fc43c4e1b33b1d01f62e1f4ac56f7c6
JohnGastone <johngastone11@gmail.com>
2024-06-11 14:16:25 +0300

    Hello World Dart

---
+++ Dart/helloWorld.dart
@@ -0,0 +1,13 @@
+class HelloWorld {
+  String hello() {
+    return "Hello, World!";
+  }
+}
+
+void main() {
+  // Creating instance of HelloWorld
+  HelloWorld hw = HelloWorld();
+
+  // Calling and printing Hello World
+  print(hw.hello());
+}
